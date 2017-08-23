67cab8cc51a130afd63b2d11671c0c528d043317
小胖哒 <chengyantaook@gmail.com>
2017-08-23 09:24:29 +0800

    Update training.py

--- training.py
+++ training.py
@@ -1,14 +1,3 @@
-#By @Kevin Xu
-#kevin28520@gmail.com
-#Youtube: https://www.youtube.com/channel/UCVCSn4qQXTDAtGWpWAe4Plw
-#
-#The aim of this project is to use TensorFlow to process our own data.
-#    - input_data.py:  read in data and generate batches
-#    - model: build the model architecture
-#    - training: train
-
-# I used Ubuntu with Python 3.5, TensorFlow 1.0*, other OS should also be good.
-# With current settings, 10000 traing steps needed 50 minutes on my laptop.
 
 # data: cats vs. dogs from Kaggle
 # Download link: https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/data
@@ -82,9 +71,7 @@ def run_training():
                
             if step % 50 == 0:
                 print('Step %d, train loss = %.2f, train accuracy = %.2f%%' %(step, tra_loss, tra_acc*100.0))
-                #summary_str = sess.run(summary_op)
-                #train_writer.add_summary(summary_str, step)
-            
+                         
             if step % 2000 == 0 or (step + 1) == MAX_STEP:
                 checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
                 saver.save(sess, checkpoint_path, global_step=step)
@@ -97,76 +84,6 @@ def run_training():
     coord.join(threads)
     sess.close()
     
-
-#%% Evaluate one image
-# when training, comment the following codes.
-
-
-#from PIL import Image
-#import matplotlib.pyplot as plt
-#
-#def get_one_image(train):
-#    '''Randomly pick one image from training data
-#    Return: ndarray
-#    '''
-#    n = len(train)
-#    ind = np.random.randint(0, n)
-#    img_dir = train[ind]
-#
-#    image = Image.open(img_dir)
-#    plt.imshow(image)
-#    image = image.resize([208, 208])
-#    image = np.array(image)
-#    return image
-#
-#def evaluate_one_image():
-#    '''Test one image against the saved models and parameters
-#    '''
-#    
-#    # you need to change the directories to yours.
-#    train_dir = '/home/kevin/tensorflow/cats_vs_dogs/data/train/'
-#    train, train_label = input_data.get_files(train_dir)
-#    image_array = get_one_image(train)
-#    
-#    with tf.Graph().as_default():
-#        BATCH_SIZE = 1
-#        N_CLASSES = 2
-#        
-#        image = tf.cast(image_array, tf.float32)
-#        image = tf.image.per_image_standardization(image)
-#        image = tf.reshape(image, [1, 208, 208, 3])
-#        logit = model.inference(image, BATCH_SIZE, N_CLASSES)
-#        
-#        logit = tf.nn.softmax(logit)
-#        
-#        x = tf.placeholder(tf.float32, shape=[208, 208, 3])
-#        
-#        # you need to change the directories to yours.
-#        logs_train_dir = '/home/kevin/tensorflow/cats_vs_dogs/logs/train/' 
-#                       
-#        saver = tf.train.Saver()
-#        
-#        with tf.Session() as sess:
-#            
-#            print("Reading checkpoints...")
-#            ckpt = tf.train.get_checkpoint_state(logs_train_dir)
-#            if ckpt and ckpt.model_checkpoint_path:
-#                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-#                saver.restore(sess, ckpt.model_checkpoint_path)
-#                print('Loading success, global_step is %s' % global_step)
-#            else:
-#                print('No checkpoint file found')
-#            
-#            prediction = sess.run(logit, feed_dict={x: image_array})
-#            max_index = np.argmax(prediction)
-#            if max_index==0:
-#                print('This is a cat with possibility %.6f' %prediction[:, 0])
-#            else:
-#                print('This is a dog with possibility %.6f' %prediction[:, 1])
-
-
-#%%
-
 
 
 
